7bc927ef85edb5b0f89be148886ea948da22e4c1
Riccardo Tasso <riccardo.tasso@gmail.com>
2020-03-08 00:38:37 +0100

    new script to read dpc data, change in palette and readme

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 
 L'associazione OnData sta facendo in questi giorni un lavoro che reputo di inestimabile valore: pubblicare i dati grezzi sulla diffusione del COVID-19 in Italia, partendo dai PDF pubblicati dalla Protezione Civile. I PDF sono comodi per i giornalisti ma non per chi vuole lavorare con i dati i modo automatico, come me. Grazie al lavoro di OnData ho potuto creare una visualizzazione che non salverà la vita a nessuno ma ci renderà più consapevoli. Ogni giorno, disponibilità dei dati permettendo, OnData aggiornerà i file e io la visualizzazione. Possibile che nel 2020 la Pubblica Amministrazione non abbia ancora capito che l'accesso al dato favorisce maggior riuso dell'informazione ufficiale?
 
+[AGGIORNAMENTO DEL 7 MARZO] grazie alla Protezione Civile abbiamo i dati ufficiali e a partire dal 24 Febbraio:
+* https://github.com/pcm-dpc/COVID-19
+
+Da oggi quindi il lavoro di OnData non è più aggiornato e anche questo script passa alla versione nuova dei dati.
+
 La visualizzazione è prodotta con il software di Flourish Studio e il link pubblico è il seguente:
 * https://public.flourish.studio/visualisation/1514619/
 
@@ -14,14 +19,8 @@ Si può embeddare in una pagina html con questo codice:
 </div>
 ```
 
-Per la disponibilità dei dati si ringrazia OnData:
-* https://github.com/ondata/covid19italia/
-* https://ondata.it/
-
-Per la qualità dei dati si fa riferimento alla Protezione Civile:
-* http://www.protezionecivile.gov.it/attivita-rischi/rischio-sanitario/emergenze/coronavirus/
-
 Ringraziamenti:
+* <a href="https://ondata.it/">OnData</a>
 * Erika Marconato
 * Matteo Brunati
 * Nicola Vitucci
---
+++ prepare_data_dpc.py
@@ -0,0 +1,67 @@
+from collections import defaultdict
+
+from urllib.request import urlopen
+
+URL = 'https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-province/dpc-covid19-ita-province.csv'
+
+f = urlopen(URL)
+content = f.read().decode('utf-8')
+
+data = {}
+
+datetimes = set()
+
+first_line = True
+for line in content.splitlines():
+	#line = str(line)
+	#print(line)
+
+	if first_line:
+		first_line = False
+		continue
+
+	# TODO: logica per skippare una linea
+
+	if 'verifica' in line.lower():
+		continue
+	if 'aggiorna' in line.lower():
+		continue
+
+	#provincia, numero, regione, datetime = line.split(',')
+	d, stato, codice_regione, denominazione_regione, codice_provincia, denominazione_provincia, sigla_provincia,lat, lng, totale_casi = line.split(',')
+
+
+	provincia = denominazione_provincia.capitalize()
+	numero = totale_casi
+	regione = denominazione_regione.capitalize()
+	datetime = d.split(' ')[0]
+
+	# TODO: logica per skippare una linea in base a qualche valore non valido
+
+	datetimes.add(datetime)
+
+	if provincia not in data:
+		data[provincia] = {}
+	
+	data[provincia][datetime] = numero
+	data[provincia]['regione'] = regione
+
+datetimes = sorted(datetimes)
+
+# HEADER
+print("provincia,regione," + ",".join(datetimes))
+
+datetime_history = defaultdict(int)
+for provincia in data:
+	line = provincia + ',' + data[provincia]['regione']
+
+	for datetime in datetimes:
+		if datetime in data[provincia]:
+			line = line + ',' + str(data[provincia][datetime])
+			datetime_history[provincia] = str(data[provincia][datetime])
+		else:
+			# se per caso mi manca il dato uso l'ultimo disponibile
+			# se non c'è l'ultimo dato disponibile uso zero perché è defaultdict
+			line = line + ',' + str(datetime_history[provincia])
+
+	print(line)
